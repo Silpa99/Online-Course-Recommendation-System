940f963d2f945019622f3f3dd7c243d2e8914b82
Silpa <silpats99@gmail.com>
2021-06-15 18:16:42 +0530

    final

--- recommender_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-admin.site.register(Student) 
-admin.site.register(Course) 
-admin.site.register(Chapter) 
-admin.site.register(Rating) 
